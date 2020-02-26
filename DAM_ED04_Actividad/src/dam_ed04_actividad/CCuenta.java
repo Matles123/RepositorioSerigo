e9aac813da0fccba0790955434b0308c7c480cde
sergi <sergi@DESKTOP-G4FOSNM>
2020-02-26 13:48:40 +0100

    primer commit

---
+++ DAM_ED04_Actividad/src/dam_ed04_actividad/CCuenta.java
@@ -0,0 +1,150 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dam_ed04_actividad;
+
+/**
+ * 
+ * @author sergi
+ */
+public class CCuenta {
+
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInterés;
+
+    public CCuenta()
+    {
+    }
+    
+/**
+ * 
+ * @param nom
+ * @param cue
+ * @param sal
+ * @param tipo 
+ */
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        nombre =nom;
+        cuenta=cue;
+        saldo=sal;
+        tipoInterés=tipo;
+    }
+/**
+ * 
+ * @param nom 
+ */
+    public void asignarNombre(String nom)
+    {
+        setNombre(nom);
+    }
+
+    public String obtenerNombre()
+    {
+        return getNombre();
+    }
+
+/**
+ * 
+ * @return 
+ */
+    public double estado()
+    {
+        return saldo;
+    }
+
+/**
+ * 
+ * @param cantidad
+ * @throws Exception 
+ */
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        saldo = saldo + cantidad;
+    }
+/**
+ * 
+ * @param cantidad
+ * @throws Exception 
+ */
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        saldo = saldo - cantidad;
+    }
+
+/**
+ * 
+ * @return 
+ */
+    public String obtenerCuenta()
+    {
+        return cuenta;
+    }
+/**
+ * 
+ * @return 
+ */
+    public String getNombre() {
+        return nombre;
+    }
+/**
+ * 
+ * @param nombre 
+ */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+/**
+ * 
+ * @return 
+ */
+    public String getCuenta() {
+        return cuenta;
+    }
+/**
+ * 
+ * @param cuenta 
+ */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+/**
+ * 
+ * @return 
+ */
+    public double getSaldo() {
+        return saldo;
+    }
+/**
+ * 
+ * @param saldo 
+ */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+/**
+ * 
+ * @return 
+ */
+    public double getTipoInterés() {
+        return tipoInterés;
+    }
+/**
+ * 
+ * @param tipoInterés 
+ */
+    public void setTipoInterés(double tipoInterés) {
+        this.tipoInterés = tipoInterés;
+    }
+
+}
